fce7c879e1224985cc6ad526b42e283c1047923f
Perumalla Venkata Naga Dhansush <perumalladhanush102@gmail.com>
2024-07-13 12:25:47 +0530

    Model Trainner Completed

--- src/components/data_injestion.py
+++ src/components/data_injestion.py
@@ -6,6 +6,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -46,4 +49,7 @@ if __name__ == '__main__':
 
     data_trasformation = DataTransformation()
 
-    data_trasformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_trasformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainner = ModelTrainer()
+    print(model_trainner.initiate_model_trainer(train_arr,test_arr))
